a19aef745574cad0f52226095bc5609afc0f9154
Evgenii <evtatdevelop@gmail.com>
2021-04-08 15:04:22 +0700

    bodified config webpack, added copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: { app: './src/index.js' },
@@ -52,13 +53,28 @@ module.exports = {
         type: "asset/resource",
 
       },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {
+      //         name: '[name].[ext]',
+      //         outputPath: 'img',
+      //       },
+      //     },
+      //   ],
+      // },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: './img',
-        },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+            }
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
@@ -81,6 +97,11 @@ module.exports = {
       title: 'Test Task',
       template: 'src/index.html',
       favicon: 'src/favicon.ico',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/img', to: 'images' },
+      ],
     }),
   ],
   mode: 'development',
