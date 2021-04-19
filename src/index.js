16d6bbaca1df6eb6e000ea74b4a5e3e39d562a55
Evgenii <evtatdevelop@gmail.com>
2021-04-19 16:38:35 +0700

    Sorting select element js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'regenerator-runtime/runtime';
 import './css/normalize.css';
 import './css/style.css';
 import './css/header.css';
+import './css/tools.css';
 import './css/bestseller.css';
 import './css/clicker.css';
 import './css/button.css';
@@ -33,3 +34,29 @@ document.addEventListener('click', (e) => {
 navigationAccount.querySelector('.account-nav > li:first-child').onclick = () => {
   document.querySelector('.navigation__account > a').focus();
 }
+
+/**
+ * Sorting Select
+ */
+const sortButton = document.querySelector('.sort-button');
+const sortListBox = document.querySelector('.sort-list-box');
+const sortList = document.querySelector('.sort-list');
+
+sortButton.addEventListener('click', () => {
+  sortListBox.classList.remove('sort-list-closed');
+  // sortList.querySelector('li:nth-child(2) input').focus();
+  // console.log(sortList.querySelector('li:nth-child(2) input'))
+})
+
+sortList.addEventListener('click', (e) => {
+  if (e.target.tagName === 'INPUT') {
+    sortButton.textContent = e.target.value;
+    sortList.querySelector('li:first-child').textContent = e.target.value;
+    sortList.querySelectorAll('li').forEach(item => {
+      item.classList.remove('sort-item-selected');
+    })
+    e.target.parentElement.classList.add('sort-item-selected');
+  }
+
+  sortListBox.classList.add('sort-list-closed');
+})
